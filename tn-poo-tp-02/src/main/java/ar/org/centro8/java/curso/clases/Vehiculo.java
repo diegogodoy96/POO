9b7ba98d6e42f082598fcba51e38bc5b0ea224df
Diego <108250452+diegogodoy96@users.noreply.github.com>
2025-10-22 13:46:46 -0300

    Update Vehiculo.java

--- tn-poo-tp-02/src/main/java/ar/org/centro8/java/curso/clases/Vehiculo.java
+++ tn-poo-tp-02/src/main/java/ar/org/centro8/java/curso/clases/Vehiculo.java
@@ -12,10 +12,11 @@ public abstract class Vehiculo implements Comparable<Vehiculo>{
 
     //Compara el objeto con un objeto específico por orden.
     //Retorna un entero negativo, cero, o un entero positivo.
-    //as this object is less than, equal to, or greater than the specified object.
+    //En caso de que el objeto sea menor, igual a, o mayor que el objeto específico
     public int compareTo(Vehiculo vehiculoSiguiente) {
         String vehiculoOriginal = this.marca + " " + this.modelo + " " + this.precio;
         String vehiculoAComparar = vehiculoSiguiente.marca + " " + vehiculoSiguiente.modelo + " " + vehiculoSiguiente.precio;
         return vehiculoOriginal.compareTo(vehiculoAComparar);
     }
 }
+
